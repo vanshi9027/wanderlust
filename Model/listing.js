4d55a8b9cc66aedca128e9a67ff204bb301f4129
Vanshika Pal <paln6823@gmail.com>
2026-05-24 22:50:28 +0530

    image upload features

--- Model/listing.js
+++ Model/listing.js
@@ -13,29 +13,16 @@ const listingSchema = new Schema({
     image: {
     filename: {
       type: String,
-      default: "listingimage",
+  
     },
     url: {
       type: String,
-      default:
-        "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?auto=format&fit=crop&w=800&q=60",
-    },
-
+     
   },
-    
-    
-    
-    // only work in string formt data in image schmea
+},
 
-    // image: {
-    //     type:  String,
-    //     default: "https://unsplash.com/photos/the-sun-shines-through-the-trees-on-a-road-s7prv0eGDNg",
-    //     set :(v) =>
-    //      v === " "
-    //      ? "https://unsplash.com/photos/the-sun-shines-through-the-trees-on-a-road-s7prv0eGDNg" 
-    //      : v,
-
-    // },
+    
+   
     price : Number,
     location : String,
     country : String,
--- routes/listings.js
+++ routes/listings.js
@@ -6,12 +6,18 @@ const Listing = require("../Model/listing.js"); //
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingControllers = require("../controllers/listings.js");
 
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 
 router
     .route("/")
     .get(wrapAsync(listingControllers.index))
-    .post(isLoggedIn, validateListing, wrapAsync(listingControllers.createListing));
+     .post(isLoggedIn , validateListing , upload.single('listing[image]') , wrapAsync(listingControllers.createListing));
+     
+ 
+   
 
 
 
